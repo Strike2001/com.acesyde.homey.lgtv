003dc745fa60807cc3563f46379d076fc9546beb
Pierre-Emmanuel Mercier <pierreemmanuel.mercier@orange.com>
2019-11-07 10:45:29 +0100

    add capabilities

--- drivers/webos/device.js
+++ drivers/webos/device.js
@@ -2,12 +2,47 @@
 
 const Homey = require('homey');
 
-class MyDevice extends Homey.Device {
+class WebOSDevice extends Homey.Device {
 	
 	onInit() {
-		this.log('MyDevice has been inited');
+		this.log('WebOSDevice has been inited');
+
+		this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
+		this.registerCapabilityListener('channel_down', this.onCapabilityChannelDown.bind(this));
+		this.registerCapabilityListener('channel_up', this.onCapabilityChannelUp.bind(this));
+		this.registerCapabilityListener('volume_set', this.onCapabilityVolumeSet.bind(this));
+		this.registerCapabilityListener('volume_down', this.onCapabilityVolumeDown.bind(this));
+		this.registerCapabilityListener('volume_mute', this.onCapabilityVolumeMute.bind(this));
+		this.registerCapabilityListener('volume_up', this.onCapabilityVolumeUp.bind(this));
 	}
-	
+
+	async onCapabilityOnoff(value, opts) {
+
+	}	
+
+	async onCapabilityChannelDown(value, opts) {
+
+	}
+
+	async onCapabilityChannelUp(value, opts) {
+
+	}
+
+	async onCapabilityVolumeSet(value, opts) {
+
+	}
+
+	async onCapabilityVolumeDown(value, opts) {
+
+	}
+
+	async onCapabilityVolumeMute(value, opts) {
+
+	}
+
+	async onCapabilityVolumeUp(value, opts) {
+
+	}
 }
 
-module.exports = MyDevice;
+module.exports = WebOSDevice;
--- drivers/webos/driver.js
+++ drivers/webos/driver.js
@@ -2,12 +2,34 @@
 
 const Homey = require('homey');
 
-class MyDriver extends Homey.Driver {
-	
+class WebOSDriver extends Homey.Driver {
+
 	onInit() {
 		this.log('MyDriver has been inited');
 	}
-	
+
+	onPairListDevices(data, callback) {
+		const devices = [
+			{
+				// Required properties:
+				"data": { "id": "abcd" },
+
+				// Optional properties, these overwrite those specified in app.json:
+				// "name": "My Device",
+				// "icon": "/my_icon.svg", // relative to: /drivers/<driver_id>/assets/
+				// "capabilities": [ "onoff", "dim" ],
+				// "capabilitiesOptions: { "onoff": {} },
+
+				// Optional properties, device-specific:
+				// "store": { "foo": "bar" },
+				// "settings": { "my_setting": "my_value" },
+
+			}
+		]
+
+		callback(null, devices);
+	}
+
 }
 
-module.exports = MyDriver;
+module.exports = WebOSDriver;
